4e9c94bb7680140221809a56ed37bc07947e1d0e
fachriansyahmni <fachriansyahmni@gmail.com>
2020-10-18 16:53:42 +0700

    commit

---
+++ src/pboif2_10119065_latihan13_relational/PBOIF2_10119065_Latihan13_Relational.java
@@ -0,0 +1,22 @@
+/*
+ * @author
+ * NAMA                 : Fachriansyah Muhammad Nur Ihsan
+ * KELAS                : PBOIF2
+ * NIM                  : 10119065
+ * Deskripsi Program    : Program ini berisi relational
+ *
+ */
+package pboif2_10119065_latihan13_relational;
+public class PBOIF2_10119065_Latihan13_Relational {
+    public static void main(String[] args) {
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("b >= a = " + (bil2 >= bil1));
+        System.out.println("b <= a = " + (bil2 <= bil1));
+    }
+    
+}
